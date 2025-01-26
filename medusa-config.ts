11f2de53f4c6287822b8be4d17de32159069f938
Musa Musa <musabrillz@gmail.com>
2025-01-26 17:54:58 +0100

    updated configurations

--- medusa-config.ts
+++ medusa-config.ts
@@ -5,6 +5,9 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
+    redisUrl: process.env.REDIS_URL,
+    
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
