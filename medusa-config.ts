46d66280f3f95d72eefba4565479c17a430503eb
Musa Musa <musabrillz@gmail.com>
2025-01-26 19:06:21 +0100

    updated modules with medusa-payment-paystack

--- medusa-config.ts
+++ medusa-config.ts
@@ -7,7 +7,7 @@ module.exports = defineConfig({
     databaseUrl: process.env.DATABASE_URL,
     workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
     redisUrl: process.env.REDIS_URL,
-    
+
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
@@ -15,5 +15,22 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
-  }
+  },
+
+  modules: [
+    {
+      resolve: "@medusajs/medusa/payment",
+      options: {
+        providers: [
+          // other payment providers like stripe, paypal etc
+          {
+            resolve: "medusa-payment-paystack",
+            options: {
+              secret_key: process.env.PAYSTACK_SECRET_KEY!,
+            } satisfies import("medusa-payment-paystack").PluginOptions,
+          },
+        ],
+      },
+    },
+  ],
 })
